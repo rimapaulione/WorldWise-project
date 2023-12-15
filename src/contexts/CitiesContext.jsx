b911b3145a8fc383edd39f46af1f6063166fca5c
rimapaulione <rimapaulione@gmail.com>
2023-12-15 13:07:36 +0200

    edit URL

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -6,7 +6,7 @@ import {
   useCallback,
 } from "react";
 
-const BASE_URL = "http://localhost:9000";
+const BASE_URL = "https://react-project-worldwise.netlify.app";
 
 const CitiesContext = createContext();
 
